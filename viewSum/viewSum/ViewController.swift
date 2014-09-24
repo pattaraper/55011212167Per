31cb4bf7f663e9027f04d9e99f46d69e81827d2a
55011212167Per <IMac@MSU>
2014-09-24 15:48:33 +0700

    send work
    บวกค่าตามที่เรากด

--- viewSum/viewSum/ViewController.swift
+++ viewSum/viewSum/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var label3: UILabel!
+    var One = 0
+    var Two = 0
+    var Three = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +25,27 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func acbutton1(sender: AnyObject) {
+        One += 1
+        label1.text = "\(One)"
+    }
+    @IBAction func acbutton2(sender: AnyObject) {
+        Two += 1
+        label2.text = "\(Two)"
+    }
+    @IBAction func acbutton3(sender: AnyObject) {
+        Three += 1
+        label3.text = "\(Three)"
+    }
+    @IBAction func acbuttonReset(sender: AnyObject) {
+        One = 0
+        Two = 0
+        Three = 0
+        label1.text = "\(One)"
+        label2.text = "\(Two)"
+        label3.text = "\(Three)"
     }
 
 
