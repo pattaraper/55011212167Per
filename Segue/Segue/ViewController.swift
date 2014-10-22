77fcbeffea43f074b3e89c93e9d51bb43fbe6abf
Pattara <pattara_pe12@hotmail.com>
2014-10-22 16:09:44 +0700

    Modify code

--- Segue/Segue/ViewController.swift
+++ Segue/Segue/ViewController.swift
@@ -1,12 +1,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,ColorTwoViewControllerDelegate {
 
     @IBOutlet var colorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    func myVCDidFinish(controller: colorTwoViewController, text: String) {
+        colorLabel.text = "Co: " + text
+        controller.navigationController?.popViewControllerAnimated(true)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "mySegue"){
+            let vc = segue.destinationViewController as colorTwoViewController
+            vc.colorString = colorLabel.text!
+            vc.delegate = self
+        }
     }
     
 }
--- Segue/Segue/colorTwoViewController.swift
+++ Segue/Segue/colorTwoViewController.swift
@@ -7,21 +7,57 @@
 //
 
 import UIKit
+protocol ColorTwoViewControllerDelegate{
+    func myVCDidFinish(controller: colorTwoViewController,text:String)
+}
 
 class colorTwoViewController: UIViewController {
 
-    @IBOutlet var colorLable: UILabel!
+    @IBOutlet var red: UIButton!
+    @IBOutlet var green: UIButton!
+    @IBOutlet var blue: UIButton!
+    
+    @IBOutlet weak var colorLable: UILabel!
+    var delegate:ColorTwoViewControllerDelegate? = nil
     var colorString = ""
     
-    @IBAction func colorSelectionButton(sender: UIButton) {
     
+    @IBAction func colorSelectionButton(sender: UIButton) {
+        colorLable.text = sender.titleLabel!.text!
+      
+       if(colorLable.text == "Red"){
+            colorLable.textColor = UIColor.redColor()
+       }
+         if(colorLable.text == "Random"){
+            
+            colorLable.textColor = getRandomColor()
+            
+        }
+        if(colorLable.text == "Blue"){
+            colorLable.textColor = UIColor.blueColor()
+        }
+    }
+    func getRandomColor() -> UIColor{
+        
+        var randomRed:CGFloat = CGFloat(drand48())
+        
+        var randomGreen:CGFloat = CGFloat(drand48())
+        
+        var randomBlue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        
     }
     @IBAction func saveColor(sender: AnyObject) {
-        
+        if(delegate != nil){
+            delegate!.myVCDidFinish(self, text: colorLable!.text!)
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        colorLable.text = colorString
+        colorLable.layer.borderWidth = 2
+        colorLable.layer.borderColor = UIColor.blackColor().CGColor
         // Do any additional setup after loading the view.
     }
 
