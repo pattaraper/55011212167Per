40fa446c956e18cf37ae00f7371558f9c1c03904
Pattara <pattara_pe12@hotmail.com>
2014-10-31 16:43:08 +0700

    Mysegue+CoreData

---
+++ seguepluscoredata/seguepluscoredata/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  seguepluscoredata
+//
+//  Created by student on 10/31/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ seguepluscoredata/seguepluscoredata/addToViewController.swift
@@ -0,0 +1,17 @@
+//
+//  addToViewController.swift
+//  seguepluscoredata
+//
+//  Created by student on 10/31/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class addToViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
